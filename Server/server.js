be08206d158764f52ccda1964340383103ee8585
TAHAWY <amer.zp582@gmail.com>
2022-04-17 17:26:40 +0200

    1:24

--- Server/server.js
+++ Server/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import connectDatabase from "./config/MongoDB.js";
 import importData from "./DataImport.js";
 import productRoute from "./Routes/ProductRoute.js";
+import { notFound, errorHandler } from "./Middleware/Errors.js";
 
 dotenv.config();
 connectDatabase();
@@ -12,9 +13,9 @@ const app = express();
 app.use("/api/import", importData);
 app.use("/api/products", productRoute);
 
-app.get("/", (req, res) => {
-  res.send("App Is Ready");
-});
+// ERROR HANDLER
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 1000;
 
